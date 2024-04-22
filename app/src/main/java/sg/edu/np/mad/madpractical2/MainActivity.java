cd8b7f0963ab0ab60eff24153c5f50c2f4b5961e
Ethan-Chew <ethanchewmh@gmail.com>
2024-04-22 14:30:36 +0800

    feat: app UI

---
+++ app/src/main/java/sg/edu/np/mad/madpractical2/MainActivity.java
@@ -0,0 +1,36 @@
+package sg.edu.np.mad.madpractical2;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button1, button2, button3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void addButtonListener() {
+        button1 = findViewById(R.id.button);
+    }
+}
+
+class User {
+    String name;
+    String description;
+    int id;
+    boolean followed;
+}
